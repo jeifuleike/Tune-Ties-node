ccd07f2daa114eff05bafda6f98ec5221e5696d2
jeifu <2572082102@qq.com>
2024-01-03 00:40:05 +0800

    完成注册后默认添加用户信息

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,18 @@ const jwt = require('jsonwebtoken');
 var express = require('express');
 var router = express.Router();
 
+// 具体修改用户信息函数
+function addUserInfo(req, data) {
+  sql = 'INSERT INTO usersInfo (userId, userName, sex, avatar, birthday, region, label) VALUES (?, ?, ?, ?, ?, ?, ?)';
+  const value = Object.values(data)
+  req.db.query(sql, value, (err, results, fields) => {
+    if (err) {
+      console.error('Error inserting data:', err);
+    } 
+  })
+}
+
+// 注册
 router.post('/register', function(req, res, next) {
   const userName = req.body.userName;
   const password = req.body.password;
@@ -22,12 +34,28 @@ router.post('/register', function(req, res, next) {
         const userId = insertResults.insertId;
         // 创建 token
         const token = jwt.sign({ userId, userName }, 'your-secret-key', { expiresIn: '1h' });
-        res.send({ state: 1, msg: '注册成功', data: { userName, password, token } });
+
+        // 给新创建的用户加上默认用户信息
+        const userInfo = {
+          userId,
+          userName,
+          sex: 1,
+          avatar: `${req.protocol}://${req.get('host')}/public/images/defaultUser.png`,
+          birthday: new Date('1990-01-01').getTime(),
+          region: '',
+          label: ''
+        }
+        addUserInfo(req, userInfo)
+        // 发送token
+        res.send({ state: 1, msg: '注册成功', data: { token } });
       });
     }
   });
 });
 
+// 修改用户信息
+
+// 获取用户信息
 router.get('/userInfo', function(req, res, next) {
   console.log(req.query.name)
   res.send('give userInfo')
