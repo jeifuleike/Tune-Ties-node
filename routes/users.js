8d976b083abbd8b79b7d771eb8e2e2b52a8b1c7e
jeifu <2572082102@qq.com>
2024-01-31 19:46:17 +0800

    对用户模块部分接口进行了改写

--- routes/users.js
+++ routes/users.js
@@ -13,11 +13,56 @@ function addUserInfo(req, data) {
     } 
   })
 }
+
+function getUserInfo(id, req, callback) {
+  const getUserInfoQuery = 'SELECT * FROM usersInfo WHERE userId = ?';
+  req.db.query(getUserInfoQuery, [id], (err, results) => {
+    if (err) {
+      console.error('Error fetching user info:', err);
+      callback(err, null);
+    } else {
+      if (results.length > 0) {
+        // 用户信息存在，返回用户信息
+        const userInfo = results[0];
+        userInfo.label = userInfo.label ? userInfo.label.split(',') : [];
+        userInfo.listLike = userInfo.listLike ? userInfo.listLike.split(',') : [];
+        // 图片信息是否为空，为空传入默认图片，不为空拼接服务器头部
+        if (userInfo.avatar) {
+          userInfo.avatar = `${req.serverBaseUrl}/${userInfo.avatar}`;
+        } else {
+          userInfo.avatar = `${req.serverBaseUrl}/public/images/userImage/defaultUser.png`;
+        }
+        console.log(userInfo, 'userInfo')
+        callback(null, userInfo);
+      } else {
+        // 用户信息不存在
+        callback(null, null);
+      }
+    }
+  });
+}
+
+// 获取用户信息
+router.get('/userInfo', verifyToken, function(req, res, next) {
+  const userId = req.userId;
+  // 查询数据库以获取用户信息
+  getUserInfo(userId, req, (err, userInfo) => {
+    if (userInfo) {
+      res.status(200).json({ state: 1, msg: '获取用户信息成功！', data: userInfo });
+    } else {
+      // 用户信息不存在
+      res.status(404).json({ state: 0, msg: '未找到该用户', data: null });
+    }
+  });
+});
+
 // 注册
 router.post('/register', function(req, res, next) {
   const userName = req.body.userName;
   const password = req.body.password;
-
+  if (userName.length < 3) {
+    res.send({ state: 1, msg: '注册失败' });
+  }
   // 检查数据库中是否存在相同用户名的用户
   const checkUserQuery = 'SELECT * FROM users WHERE userName = ?';
 
@@ -48,41 +93,8 @@ router.post('/register', function(req, res, next) {
         }
         addUserInfo(req, userInfo)
         // 发送token
-        res.send({ state: 1, msg: '注册成功', data: { token } });
+        res.send({ state: 1, msg: '注册成功', data: { token, userInfo } });
       });
-    }
-  });
-});
-
-// 修改用户信息
-
-// 获取用户信息
-router.get('/userInfo', verifyToken, function(req, res, next) {
-  const userId = req.userId;
-  // 查询数据库以获取用户信息
-  const getUserInfoQuery = 'SELECT * FROM usersInfo WHERE userId = ?';
-
-  req.db.query(getUserInfoQuery, [userId], (err, results) => {
-    if (err) {
-      console.error('Error fetching user info:', err);
-      res.status(500).json({ state: 0, msg: 'Internal Server Error', data: null });
-    } else {
-      if (results.length > 0) {
-        // 用户信息存在，返回用户信息
-        const userInfo = results[0];
-        userInfo.label = userInfo.label? userInfo.label.split(',') : []
-        userInfo.listLike = userInfo.listLike? userInfo.listLike.split(',') : []
-        // 图片信息是否为空，为空传入默认图片，不为空拼接服务器头部
-        if (userInfo.avatar) {
-          userInfo.avatar = `${req.serverBaseUrl}/${userInfo.avatar}`
-        } else {
-          userInfo.avatar = `${req.serverBaseUrl}/public/images/userImage/defaultUser.png`
-        }
-        res.status(200).json({ state: 1, msg: '获取用户信息成功！', data: userInfo });
-      } else {
-        // 用户信息不存在
-        res.status(404).json({ state: 0, msg: '未找到该用户', data: null });
-      }
     }
   });
 });
@@ -103,15 +115,16 @@ router.post('/login', function(req, res, next) {
         // 密码匹配，生成token
         const userId = results[0].id;
         const token = jwt.sign({ userId, userName }, 'Tune-Ties', { expiresIn: '7d' });
-
-        res.send({ state: 1, msg: '登录成功', data: { token } });
+        getUserInfo(userId, req, (err, userInfo) => {
+          res.send({ state: 1, msg: '登录成功', data: { token, userInfo } });
+        });
       } else {
         // 密码错误
-        res.status(402).json({ state: 0, msg: '密码错误！', data: null });
+        res.status(404).json({ state: 0, msg: '密码错误！', data: null });
       }
     } else {
       // 用户不存在
-      res.status(402).json({ state: 0, msg: '用户不存在！', data: null });
+      res.status(404).json({ state: 0, msg: '用户不存在！', data: null });
     }
   });
 });
@@ -145,7 +158,7 @@ router.post('/changeUserInfo', verifyToken, function(req, res, next) {
       console.error('Error updating user info:', err);
       res.status(500).json({ state: 0, msg: 'Internal Server Error', data: null });
     } else {
-      res.status(200).json({ state: 1, msg: 'User info updated successfully', data: null });
+      res.status(200).json({ state: 1, msg: 'User info updated successfully', data: userInfo });
     }
   });
 });
